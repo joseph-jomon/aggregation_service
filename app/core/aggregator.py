548e099fd19bbe73151aa33de5818198ee434d4d
joseph-jomon <josephjomon401@gmail.com>
2024-10-06 15:24:16 +0200

    class to check if both embeddings are present and if so to ingest the combined data in to the databse

--- app/core/aggregator.py
+++ app/core/aggregator.py
@@ -2,8 +2,9 @@ from app.services.redis_service import save_embedding
 from app.services.ingestion_service import ingest_combined_data
 
 class Aggregator:
-    def aggregate_data(self, data_id: str, embedding_type: str, embedding: list):
+    async def aggregate_data(self, data_id: str, embedding_type: str, embedding: list):
+         # Save embedding and check if both text and image embeddings are ready
         data = save_embedding(data_id, {"id": data_id, embedding_type: embedding}, embedding_type)
         if data:
             # Send the combined data to ingestion once both embeddings are ready
-            ingest_combined_data(data)
+            await ingest_combined_data(data)

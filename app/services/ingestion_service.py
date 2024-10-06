6d96c0a6d4f3342a0fb2969b338768b8e14f4541
joseph-jomon <josephjomon401@gmail.com>
2024-10-07 00:56:31 +0200

    changed the ingestion end to send list of data combined and ready for ingestion instead of individual ids

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -1,6 +1,12 @@
 import httpx
 
-async def ingest_combined_data(data: dict):
+async def ingest_combined_data_bulk(data_list: list):
+    """
+    Ingests a batch of combined data into the database.
+
+    Args:
+        data_list (list): A list of combined data dictionaries.
+    """
     async with httpx.AsyncClient() as client:
-        response = await client.post("http://database-service:8000/ingest/{index_name}", json=data)
+        response = await client.post("http://database-service:8000/ingest/{index_name}", json=data_list)
         return response.status_code, response.json()

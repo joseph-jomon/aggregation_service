6206af459dcb384076f1321eda9e2185241e3187
joseph-jomon <josephjomon401@gmail.com>
2024-10-06 15:20:21 +0200

    specifying the routs in the app/api/routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter
 from app.core.aggregator import Aggregator
+from app.models.data_model import BatchEmbeddings
 
 router = APIRouter()
 aggregator = Aggregator()
 
-@router.post("/aggregate/")
-async def send_for_aggregation(data: dict):
-    embedding_type = data.get("embedding_type")
-    data_id = data.get("id")
-    embedding = data.get("embedding")
-
-    if not (embedding_type and data_id and embedding):
-        return {"status": "Invalid request"}
-
-    # Aggregate data in Redis
-    aggregator.aggregate_data(data_id, embedding_type, embedding)
-    return {"status": "Data sent for aggregation."}
+@router.post("/aggregate/", response_model=dict)
+async def send_for_aggregation(batch_data: BatchEmbeddings):
+    # Iterate over each embedding in the batch
+    for embedding_data in batch_data.embeddings:
+        # embedding_data.embedding_type is now correctly defined
+        aggregator.aggregate_data(embedding_data.id, embedding_data.embedding_type, embedding_data.embedding)
+    return {"status": "Batch data sent for aggregation."}

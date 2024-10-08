f32be9942303f1af69556632920d7c01dfc5ddb0
joseph-jomon <josephjomon401@gmail.com>
2024-10-08 19:42:05 +0200

    adding the index name also to the data_model

--- app/models/data_model.py
+++ app/models/data_model.py
@@ -5,6 +5,7 @@ class Embedding(BaseModel):
     id: str
     embedding_type: str  # Can be "EMBEDDINGS_TEXT" or "EMBEDDINGS_IMAGE"
     embedding: List[float]
+    index_name: str  # Add index name here to identify the company/immobilien service
 
 class BatchEmbeddings(BaseModel):
     embeddings: List[Embedding]
